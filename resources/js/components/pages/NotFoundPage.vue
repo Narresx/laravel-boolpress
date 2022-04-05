b6a25f5217098b89b6a49edb3ec4ca05de270c66
Narresx <92748159+Narresx@users.noreply.github.com>
2022-04-05 15:39:59 +0200

    managed page not found 404

---
+++ resources/js/components/pages/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h3 class="text-center">404 | Not Found</h3>
+</div>
+</template>
+
+<script>
+export default {
+    name: "PageNotFound",
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -5,12 +5,16 @@ Vue.use(VueRouter)
 
 import HomePage from './components/pages/HomePage.vue';
 import ContactPage from './components/pages/ContactPage.vue';
+import NotFoundPage from './components/pages/NotFoundPage.vue';
+
 
 const router = new VueRouter({
 mode: 'history', 
 routes: [
     {path: '/', component: HomePage},
     {path: '/contacts', component: ContactPage},
+    {path: '*', component: NotFoundPage},
+
 ]
 });
 
