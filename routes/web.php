0150af427a515bfb96a2494f91b7ba7f4924d1e4
Narresx <92748159+Narresx@users.noreply.github.com>
2022-04-06 15:22:42 +0200

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-}); 
+Auth::routes();
 
 Route::middleware('auth')->prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
