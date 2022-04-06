91ae24c57b1e2c879b2bb57c534a4b1f098f3ad9
Narresx <92748159+Narresx@users.noreply.github.com>
2022-04-06 15:18:30 +0200

    add route home

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', function () {
+    return view('guest.home');
+}); 
 
 Route::middleware('auth')->prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
